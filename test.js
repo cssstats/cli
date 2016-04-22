d5bb43dc1b5c691d8edcd39ebd3ec0454d42bc8f
John Otander <johnotander@gmail.com>
2016-04-22 15:45:39 -0600

    Scaffold out some tests

--- test.js
+++ test.js
@@ -1,8 +1,24 @@
+import fs from 'fs'
 import test from 'ava'
-import cssstatsCli from './'
+import pify from 'pify'
+import childProcess from 'child_process'
 
-test('cssstats-cli does something awesome', t => {
-  t.plan(1)
+t expected = fs.readFileSync('expected.json', 'utf8')
+const fixtureFile = 'fixture.css'
+const outputFile = 'output.json'
 
-  t.true(cssstatsCli())
+test('crunches stats', async t => {
+    t.plan(1)
+
+    const stdout = await pify(childProcess.execFile)('./cli.js', [fixtureFile])
+    t.same(stdout, expected)
+})
+
+test('writes to a file when provided', async t => {
+    t.plan(1)
+
+    await pify(childProcess.execFile)('./cli.js', [fixtureFile, outputFile])
+    const output = fs.readFileSync(outputFile, 'utf8')
+    
+    t.same(output, expected)
 })

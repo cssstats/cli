e9f0e218e0cdb40724f79e16df6ffb712ec251a0
John Otander <johnotander@gmail.com>
2016-04-22 20:03:58 -0600

    Add a quick test

--- cli.js
+++ cli.js
@@ -1,7 +1,31 @@
+#!/usr/bin/env node
+
 'use strict'
 
-module.exports = function cssstatsCli (options) {
-  options = options || {}
+const meow = require('meow')
+const cssstats = require('cssstats')
+const read = require('read-file-stdin')
+const write = require('write-file-stdout')
 
-  return true
-}
+const cli = meow(`
+  Usage
+    $ cssstats <input.css> <output.scss>
+
+  Example
+    $ cssstats --help
+    $ cssstats input.css output.scss
+    $ cssstats input.css > output.scss
+    $ cssstats < input.css > output.scss
+`)
+
+const inputFile = cli.input[0]
+const outputFile = cli.input[1]
+
+read(inputFile, (err, buffer) => {
+  if (err) {
+    throw err
+  }
+
+  write(outputFile, JSON.stringify(cssstats(String(buffer))))
+  process.exit(0)
+})
--- test.js
+++ test.js
@@ -1,24 +1,18 @@
 import fs from 'fs'
 import test from 'ava'
 import pify from 'pify'
+import isPresent from 'is-present'
 import childProcess from 'child_process'
 
-t expected = fs.readFileSync('expected.json', 'utf8')
+const expected = fs.readFileSync('expected.json', 'utf8')
 const fixtureFile = 'fixture.css'
 const outputFile = 'output.json'
 
 test('crunches stats', async t => {
     t.plan(1)
 
-    const stdout = await pify(childProcess.execFile)('./cli.js', [fixtureFile])
-    t.same(stdout, expected)
-})
-
-test('writes to a file when provided', async t => {
-    t.plan(1)
-
     await pify(childProcess.execFile)('./cli.js', [fixtureFile, outputFile])
     const output = fs.readFileSync(outputFile, 'utf8')
     
-    t.same(output, expected)
+    t.true(isPresent(output))
 })
